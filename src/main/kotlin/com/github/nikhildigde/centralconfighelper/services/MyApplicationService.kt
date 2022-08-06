7cb5a3bc5e641a4b2c2ebb82db386cd908e5a400
GitHub Action <action@github.com>
2022-08-06 07:17:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nikhildigde/centralconfighelper/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nikhildigde.centralconfighelper.services
+
+import com.github.nikhildigde.centralconfighelper.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nikhildigde/centralconfighelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nikhildigde/centralconfighelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikhildigde.centralconfighelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikhildigde.centralconfighelper.MyBundle
 
 class MyProjectService(project: Project) {
 
